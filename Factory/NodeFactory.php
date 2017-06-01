37491e12501927ae34b1d8da02fc5ea2b1287a51
Christof Hufnagel <ch@mandarin-medien.de>
2017-06-01 11:28:07 +0200

    Added NodeFactory

---
+++ Factory/NodeFactory.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace MandarinMedien\MMCmfNodeBundle\Factory;
+
+use Doctrine\ORM\EntityManagerInterface;
+use MandarinMedien\MMCmfNodeBundle\Entity\Node;
+use MandarinMedien\MMCmfNodeBundle\Entity\NodeInterface;
+
+
+class NodeFactory
+{
+
+    private $manager;
+    private $factory_class = Node::class;
+    private $meta;
+
+
+    /**
+     * ContentNodeFactory constructor.
+     * @param EntityManagerInterface $manager
+     */
+    public function __construct(EntityManagerInterface $manager)
+    {
+        $this->manager = $manager;
+        $this->meta = $this->manager->getClassMetadata(
+            $this->factory_class
+        );
+    }
+
+
+    /**
+     * create a new ContentNode instance by discriminator value
+     * @param string $discriminator
+     * @return NodeInterface
+     * @throws \Exception
+     */
+    public function createNode($discriminator = 'default')
+    {
+        $reflection = new \ReflectionClass($this->getClassByDiscriminator($discriminator));
+
+        $instance = $reflection->newInstance();
+        if($instance instanceof NodeInterface)
+        {
+            return $instance;
+        }
+
+        return null;
+    }
+
+
+    /**
+     * get all available discriminator values of Node entity
+     * @param array $exclude exclude specific discriminators
+     * @return array
+     */
+    public function getDiscriminators($exclude = array())
+    {
+
+        // prefilter discriminators by subclasses
+        $subclasses = $this->meta->subClasses;
+
+        $discriminators = array_filter(
+            $this->meta->discriminatorMap,
+            function($class) use ($subclasses)
+            {
+                return in_array($class, $subclasses);
+            }
+        );
+
+
+        // filter discriminators by exlude array
+        return array_diff(array_keys($discriminators), $exclude);
+    }
+
+
+    /**
+     * get the discriminator value by the given instance
+     * @param NodeInterface $node
+     * @return \Doctrine\ORM\Mapping\ClassMetadata
+     */
+    public function getDiscriminatorByClass(NodeInterface $node)
+    {
+        return $this->manager->getClassMetadata(get_class($node))->discriminatorValue;
+    }
+
+    /**
+     * get the discriminator value by the given className
+     * @param string $className
+     * @return \Doctrine\ORM\Mapping\ClassMetadata
+     */
+    public function getDiscriminatorByClassName($className)
+    {
+        return $this->manager->getClassMetadata($className)->discriminatorValue;
+    }
+
+
+    /**
+     * get the ContentNode subclass by discriminator value
+     * @param string $discriminator
+     * @return string
+     * @throws \Exception
+     */
+    public function getClassByDiscriminator($discriminator)
+    {
+        if ($class = ($this->meta->discriminatorMap[$discriminator])) {
+            return $class;
+        } else {
+            throw new \Exception('class not found');
+        }
+    }
+}
