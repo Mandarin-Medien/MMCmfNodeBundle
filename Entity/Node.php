94543aee7b9df2ca61be7fff8207b882dc0e0abf
Christof Hufnagel <ch@mandarin-medien.de>
2017-06-09 15:40:08 +0200

    remove unnecessary imports on Node Entity

--- Entity/Node.php
+++ Entity/Node.php
@@ -3,8 +3,6 @@
 namespace MandarinMedien\MMCmfNodeBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\ORM\Mapping as ORM;
-use MandarinMedien\MMCmfRoutingBundle\Entity\NodeRoute;
 use Symfony\Component\Validator\Constraints as Assert;
 use MandarinMedien\MMCmfNodeBundle\Validator\Constraint as NodeAssert;
 
