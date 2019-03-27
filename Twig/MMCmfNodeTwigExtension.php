e8cbe87398fd61c4d5e1bba3e4435c29f4b5e2a5
Toni Gurski <tg@mandarin-medien.de>
2019-03-27 17:03:24 +0100

    reimplemented display_classes attribute into node render method

--- Twig/MMCmfNodeTwigExtension.php
+++ Twig/MMCmfNodeTwigExtension.php
@@ -16,7 +16,6 @@ class MMCmfNodeTwigExtension extends \Twig_Extension
      */
     protected $nodeFactory;
 
-
     /**
      * @var TemplateManager
      */
@@ -27,7 +26,12 @@ class MMCmfNodeTwigExtension extends \Twig_Extension
      */
     protected $kernel;
 
-
+    /**
+     * MMCmfNodeTwigExtension constructor.
+     * @param NodeFactory $factory
+     * @param TemplateManager $manager
+     * @param KernelInterface $kernel
+     */
     public function __construct(NodeFactory $factory, TemplateManager $manager, KernelInterface $kernel)
     {
         $this->templateManager = $manager;
@@ -59,14 +63,21 @@ class MMCmfNodeTwigExtension extends \Twig_Extension
      * @param string|null $template
      * @param array $options
      * @return string
+     *
      * @throws \Twig_Error_Loader
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
+     * @throws \ReflectionException
      */
     public function render(\Twig_Environment $twig, TemplatableNodeInterface $node, string $template = null, array $options = [])
     {
+        $refClass = new \ReflectionClass($node);
+        $className = trim(str_replace($refClass->getNamespaceName(), '', $refClass->getName()), '\\');
+        $display_classes = [$className];
+
         return $twig->render($template ?: $this->templateManager->getTemplate($node), [
-            'node' => $node
+            'node' => $node,
+            'display_classes' => implode(" ", $display_classes),
         ]);
     }
 
