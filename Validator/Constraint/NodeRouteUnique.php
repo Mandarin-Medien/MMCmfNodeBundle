78019dce93c1440db78940e1315c7768b5f7fefa
Christof Hufnagel <ch@mandarin-medien.de>
2019-04-11 11:18:33 +0200

    fix constraint NodeRouteUniqiue::validateBy

--- Validator/Constraint/NodeRouteUnique.php
+++ Validator/Constraint/NodeRouteUnique.php
@@ -11,11 +11,6 @@ class NodeRouteUnique extends Constraint
 {
     public $message  = 'The route "%string%" is not unique. Look at Route ID "%routeId%"';
 
-    public function validatedBy()
-    {
-        return 'node_route_unique';
-    }
-
     public function getTargets()
     {
         return self::CLASS_CONSTRAINT;
